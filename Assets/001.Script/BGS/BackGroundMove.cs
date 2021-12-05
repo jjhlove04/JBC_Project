15730aba9a7ea55046ae75659137ea9d6532d02f
jjhlovejh04 <jjhlovejh04@naver.com>
2021-12-05 23:36:06 +0900

    BGMove

---
+++ Assets/001.Script/BGS/BackGroundMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour
+{
+    private MeshRenderer[]  bGMaterial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
