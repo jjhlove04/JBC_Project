48e2e4fb60f29dfec9444efbd01122c3d3b65e63
jjhlovejh04 <jjhlovejh04@naver.com>
2021-12-06 00:50:53 +0900

    BGMove
    배경 움직이기 완성

--- Assets/001.Script/BGS/BackGroundMove.cs
+++ Assets/001.Script/BGS/BackGroundMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BackGroundMove : MonoBehaviour
 {
-    private MeshRenderer[]  bGMaterial;
-    void Start()
-    {
-        
-    }
+    //움직일 배경의 Material을 가져와서 offset을 조정해서 움직이는것을 만든다.
+    public MeshRenderer[]  bGMaterial;
 
-    // Update is called once per frame
+    [SerializeField][Tooltip("배경을 움직이게 할 속도")]
+    private float[] offsetSpeedMenu; //0=땅, 1=강, 2=하늘
+    private float ground = 0, sky = 0, river =0;
     void Update()
     {
-        
+        BGMove();
+    }
+
+    void BGMove()
+    {
+        ground += offsetSpeedMenu[0] * Time.deltaTime;
+        river += offsetSpeedMenu[1] * Time.deltaTime;
+        sky += offsetSpeedMenu[2] * Time.deltaTime;
+
+        bGMaterial[0].material.mainTextureOffset = new Vector2(ground, 0);
+        bGMaterial[1].material.mainTextureOffset = new Vector2(river, 0);
+        bGMaterial[2].material.mainTextureOffset = new Vector2(sky, 0);
     }
 }
