bf7a9825a400d335c9b7c455de5eb4d65de4e6d9
jjhlovejh04 <jjhlovejh04@naver.com>
2021-12-06 22:56:04 +0900

    Card
    카드 리뉴얼

--- Assets/001.Script/Card/Card.cs
+++ Assets/001.Script/Card/Card.cs
@@ -1,95 +1,101 @@
-// using DG.Tweening;
-// using TMPro;
-// using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
 
-// public class Card : MonoBehaviour
-// {
-//     [SerializeField] SpriteRenderer card;
-//     [SerializeField] SpriteRenderer skillImage;
-//     [SerializeField] TMP_Text nameTMP;
-//     [SerializeField] TMP_Text damageTMP;
-//     [SerializeField] TMP_Text costTMP;
-//     [SerializeField] Sprite cardFront;
-//     [SerializeField] Sprite cardback;
+public class Card : MonoBehaviour
+{
+    [SerializeField][Tooltip("베이스 카드의 스프라이트 랜더러")] SpriteRenderer card;
+    [SerializeField][Tooltip("스킬의 이미지")] SpriteRenderer skillImage;
+    [SerializeField][Tooltip("스킬카드의 이름")]TMP_Text nameTMP;
+    [SerializeField][Tooltip("데미지 텍스트")]TMP_Text damageTMP;
+    [SerializeField][Tooltip("카드 비용 텍스트")]TMP_Text costTMP;
+    [SerializeField][Tooltip("카드 앞면")] Sprite cardFront;
+    [SerializeField][Tooltip("카드 뒷면")] Sprite cardback;
 
     
-//     public Item item;
-//     bool isFront;
-//     public PRS originPRS;
+    public Item item;
+    bool isFront;
+    //public PRS originPRS;
 
-//     public void Setup(Item item, bool isFront)
-//     {
-//         this.item = item;
-//         this.isFront = isFront;
-//         if (this.isFront)
-//         {
-//             skillImage.sprite = this.item.CardImage;
-//             nameTMP.text = this.item.name;
-//             costTMP.text = this.item.cost.ToString();
-//             damageTMP.text = this.item.damage.ToString();
-//         }
-//         else
-//         {
-//             card.sprite = cardback;
-//             nameTMP.text = "";
-//             damageTMP.text = "";
-//             costTMP.text = "";
-//         }
-//     }
+    public void Setup(Item item, bool isFront)
+    {
+        this.item = item;
+        this.isFront = isFront;
+        if (this.isFront)
+        {
+            skillImage.sprite = this.item.CardImage;
+            nameTMP.text = this.item.name;
+            costTMP.text = this.item.cost.ToString();
+            damageTMP.text = this.item.damage.ToString();
+        }
+        else
+        {
+            card.sprite = cardback;
+            nameTMP.text = "";
+            damageTMP.text = "";
+            costTMP.text = "";
+        }
+    }
 
-//     public void CheckCard()
-//     {
+    public void CheckCard()
+    {
 
-//     }
+    }
 
-//     public void MoveTransform(PRS prs, bool useDotween, float dotweenTime = 0)
-//     {
-//         if (useDotween)
-//         {
-//             transform.DOMove(prs.pos, dotweenTime);
-//             transform.DORotateQuaternion(prs.rot, dotweenTime);
-//             transform.DOScale(prs.scale, dotweenTime);
-//         }
-//         else
-//         {
-//             transform.position = prs.pos;
-//             transform.rotation = prs.rot;
-//             transform.localScale = prs.scale;
-//         }
-//     }
+    public void MoveTransform(/*PRS prs*/ bool useDotween, float dotweenTime = 0)
+    {
+        // if (useDotween)
+        // {
+        //     transform.DOMove(prs.pos, dotweenTime);
+        //     transform.DORotateQuaternion(prs.rot, dotweenTime);
+        //     transform.DOScale(prs.scale, dotweenTime);
+        // }
+        // else
+        // {
+        //     transform.position = prs.pos;
+        //     transform.rotation = prs.rot;
+        //     transform.localScale = prs.scale;
+        // }
+    }
 
-//     void Start()
-//     {
+    void Start()
+    {
 
-//     }
+    }
 
 
-//     void Update()
-//     {
+    void Update()
+    {
 
-//     }
-//     void OnMouseDown()
-//     {
-//         if (isFront)
-//             CardManager.Inst.CardMouseDown();
-//     }
+    }
+    void OnMouseDown()
+    {
+        if (isFront)
+        {
 
-//     void OnMouseUp()
-//     {
-//         if (isFront)
-//             CardManager.Inst.CardMouseUp();
-//     }
-//     void OnMouseOver()
-//     {
-//         if (isFront)
-//         {
+        }
+            //CardManager.Inst.CardMouseDown();
+    }
+
+    void OnMouseUp()
+    {
+        if (isFront)
+        {
+
+        }
+            //CardManager.Inst.CardMouseUp();
+    }
+    void OnMouseOver()
+    {
+        if (isFront)
+        {
             
-//             CardManager.Inst.CardMouseOver(this);
-//         }
-//     }
-//     void OnMouseExit()
-//     {
-//         if (isFront)
-//             CardManager.Inst.CardMouseExit(this);
-//     }
-// }
+            //CardManager.Inst.CardMouseOver(this);
+        }
+    }
+    void OnMouseExit()
+    {
+        
+           // CardManager.Inst.CardMouseExit(this);
+    }
+}
